57fa47d2b8731b315fa48daf190ba2cb5b272368
TayyabRehman124 <mtayyabrehman@gmail.com>
2024-09-18 19:22:38 +0500

    tab bar icon color on focus changed

--- src/routes/BottomTab/index.js
+++ src/routes/BottomTab/index.js
@@ -7,67 +7,86 @@ import MatchScreen from '../../screens/main/MatchScreen';
 import Octicons from 'react-native-vector-icons/Octicons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-
-const Tab = createBottomTabNavigator();
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Colors from '../../components/Colors';
 
+const Tab = createBottomTabNavigator();
+
 const BottomTab = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
         tabBarStyle: {
           height: 70,
           paddingBottom: 10,
           paddingTop: 10,
         },
-      }}>
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          let IconComponent;
+
+          switch (route.name) {
+            case 'HomeScreen':
+              IconComponent = Octicons;
+              iconName = 'home';
+              break;
+            case 'SubscriptionScreen':
+              IconComponent = Ionicons;
+              iconName = 'pricetag-outline';
+              break;
+            case 'MatchScreen':
+              IconComponent = Octicons;
+              iconName = 'heart';
+              break;
+            case 'ChatsScreen':
+              IconComponent = Ionicons;
+              iconName = 'chatbubble-ellipses-outline';
+              break;
+            case 'ProfileScreen':
+              IconComponent = FontAwesome5;
+              iconName = 'user';
+              break;
+            default:
+              return null;
+          }
+
+          // Return the icon component with dynamic color
+          return (
+            <IconComponent
+              name={iconName}
+              size={size}
+              color={focused ? Colors.orange : Colors.inactiveTabColor}
+            />
+          );
+        },
+        tabBarActiveTintColor: Colors.orange, // Active color
+        tabBarInactiveTintColor: '#9E9E9E', // Inactive color
+      })}>
       <Tab.Screen
         name="HomeScreen"
         component={HomeScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: () => <Octicons name={'home'} size={30} />,
-        }}
+        options={{headerShown: false}}
       />
-
       <Tab.Screen
         name="SubscriptionScreen"
         component={SubscriptionScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: () => <Ionicons name={'pricetag-outline'} size={30} />,
-        }}
+        options={{headerShown: false}}
       />
-
       <Tab.Screen
         name="MatchScreen"
         component={MatchScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: () => <Octicons name={'heart'} size={30} />,
-        }}
+        options={{headerShown: false}}
       />
-
       <Tab.Screen
         name="ChatsScreen"
         component={ChatsScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: () => (
-            <Ionicons name={'chatbubble-ellipses-outline'} size={30} />
-          ),
-        }}
+        options={{headerShown: false}}
       />
-
       <Tab.Screen
         name="ProfileScreen"
         component={ProfileScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: () => <FontAwesome5 name={'user'} size={30} />,
-        }}
+        options={{headerShown: false}}
       />
     </Tab.Navigator>
   );
